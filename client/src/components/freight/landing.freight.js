1a52e52adec829736ef628c237563e6b5a6da415
Cassey Shao <casseys0000@gmail.com>
2022-03-19 22:10:52 -0400

    Initial freight landing page commit

--- client/src/components/freight/landing.freight.js
+++ client/src/components/freight/landing.freight.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import './landing.freight.css'
+import { Autocomplete, TextField } from '@mui/material';
 
 export default function FreightLanding() {
   return (
-    <div>landing.freight</div>
+    <div>
+       <div className='hero'>
+         <h1>Book a flight for your freight</h1>
+         <p>PolarPool keeps things flexible and safe</p>
+       </div>
+       <br/>
+       <div className='hero'>
+         <h2>Enter freight information below: </h2>
+       </div>
+       <div className='hero'>
+          <TextField id="" label="Type of freight:" />
+          <TextField id="" label="Amount of freight:" />
+          <TextField id="" label="Urgency of delivery:" />
+       </div>
+    </div>
   )
 }
+
+
